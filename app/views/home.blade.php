a6e035142ee666ac2ca416e44e364f521f4eef35
ryanorsinger <ryan.orsinger@gmail.com>
2014-04-10 04:25:35 -0500

    working on layering images

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -40,7 +40,7 @@
 	#date-img {
 		position: absolute;
 		padding: 0.25em;
-		top: 6em; left: 0; right: 0;
+		top: 10em; left: 2em; right: 2em;
 		margin-top: 15px;
 	}
 
@@ -60,6 +60,7 @@
       		<div class="row">
         		<img class="img-responsive" alt="Responsive banner image" id="banner-img" src="/images/logo_with_parrot.png">  
            		<img class="img-responsive" id="date-img" src="/images/date.png">
+           		<img class="img-responsive" id="kw-logo" src="/images/smaller/kw_logo.png">
         	</div>
         </div>
 	<div class="row">
@@ -74,6 +75,18 @@
                   <img src="/images/logo_with_parrot.png" width="175" height="135">
                 </span>
               </a>
+              
+              <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+				<input type="hidden" name="cmd" value="_s-xclick">
+				<table>
+				<tr><td><input type="hidden" name="on0" value="Event Entry"><p>Event Entry</p></td></tr><tr><td><input type="text" name="os0" maxlength="200"></td></tr>
+				</table>
+				<input type="hidden" name="encrypted" value="-----BEGIN PKCS7-----MIIHmAYJKoZIhvcNAQcEoIIHiTCCB4UCAQExggEwMIIBLAIBADCBlDCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20CAQAwDQYJKoZIhvcNAQEBBQAEgYA77PQrF2/sT3Rt3T+Ikj5kKdTFsxUXZo0wXhVgjVup6Krt3t/4dk5rpkh0sV/GD+ZX4a0BbyEO+80Gacwa0DqFcS78oupmkaGjut0IA6E9sQBn9fgaToiG21yeq/gO2hzjAAvr83s6VPUG8q8VOpYJ+9GgXRcRN8lHJzzZY4z8lzELMAkGBSsOAwIaBQAwggEUBgkqhkiG9w0BBwEwFAYIKoZIhvcNAwcECKrcr4D6/V7OgIHw5UBpc3tMjYoMwr/ZgndF27swopcul9qD6CxRwmriKqShySjJIeOwVRck6haJsWPOE7n5CAS6RdThsgcYBWl5qfeZ9Risu9TFbgmMzmWuX3Lw1Fl3LTDuSgDUej3yG1UcQKyOqWisaTPgAsCqaRdT+ZqnLPFBonLoQB/chSyapdFAcKwUUaVdGaa1rlNo30RdIS+ZuuDMFip5VKi5neKYPcz4QrR38llJ7e32VV4xVCCcU2JuZfcokdSeTYZQEKMV6dXJ8v5tR4QgsRZYexKd+YE3KXak4eKh0d7P5EFEUTFUM6BOufUt4zRUsu2trbYVoIIDhzCCA4MwggLsoAMCAQICAQAwDQYJKoZIhvcNAQEFBQAwgY4xCzAJBgNVBAYTAlVTMQswCQYDVQQIEwJDQTEWMBQGA1UEBxMNTW91bnRhaW4gVmlldzEUMBIGA1UEChMLUGF5UGFsIEluYy4xEzARBgNVBAsUCmxpdmVfY2VydHMxETAPBgNVBAMUCGxpdmVfYXBpMRwwGgYJKoZIhvcNAQkBFg1yZUBwYXlwYWwuY29tMB4XDTA0MDIxMzEwMTMxNVoXDTM1MDIxMzEwMTMxNVowgY4xCzAJBgNVBAYTAlVTMQswCQYDVQQIEwJDQTEWMBQGA1UEBxMNTW91bnRhaW4gVmlldzEUMBIGA1UEChMLUGF5UGFsIEluYy4xEzARBgNVBAsUCmxpdmVfY2VydHMxETAPBgNVBAMUCGxpdmVfYXBpMRwwGgYJKoZIhvcNAQkBFg1yZUBwYXlwYWwuY29tMIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDBR07d/ETMS1ycjtkpkvjXZe9k+6CieLuLsPumsJ7QC1odNz3sJiCbs2wC0nLE0uLGaEtXynIgRqIddYCHx88pb5HTXv4SZeuv0Rqq4+axW9PLAAATU8w04qqjaSXgbGLP3NmohqM6bV9kZZwZLR/klDaQGo1u9uDb9lr4Yn+rBQIDAQABo4HuMIHrMB0GA1UdDgQWBBSWn3y7xm8XvVk/UtcKG+wQ1mSUazCBuwYDVR0jBIGzMIGwgBSWn3y7xm8XvVk/UtcKG+wQ1mSUa6GBlKSBkTCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb22CAQAwDAYDVR0TBAUwAwEB/zANBgkqhkiG9w0BAQUFAAOBgQCBXzpWmoBa5e9fo6ujionW1hUhPkOBakTr3YCDjbYfvJEiv/2P+IobhOGJr85+XHhN0v4gUkEDI8r2/rNk1m0GA8HKddvTjyGw/XqXa+LSTlDYkqI8OwR8GEYj4efEtcRpRYBxV8KxAW93YDWzFGvruKnnLbDAF6VR5w/cCMn5hzGCAZowggGWAgEBMIGUMIGOMQswCQYDVQQGEwJVUzELMAkGA1UECBMCQ0ExFjAUBgNVBAcTDU1vdW50YWluIFZpZXcxFDASBgNVBAoTC1BheVBhbCBJbmMuMRMwEQYDVQQLFApsaXZlX2NlcnRzMREwDwYDVQQDFAhsaXZlX2FwaTEcMBoGCSqGSIb3DQEJARYNcmVAcGF5cGFsLmNvbQIBADAJBgUrDgMCGgUAoF0wGAYJKoZIhvcNAQkDMQsGCSqGSIb3DQEHATAcBgkqhkiG9w0BCQUxDxcNMTQwNDEwMDMwMjEzWjAjBgkqhkiG9w0BCQQxFgQUWicWHw0sJyXiIdoY41T/1oojBaQwDQYJKoZIhvcNAQEBBQAEgYC1e1Lk2kuia/fyAiYawlqe8qqxb6ApECZB/0qzjGeV2zhawBS9gGezXMQCZmGh1EadYz13nQg5YOeiks/5OZqAGpCTik9aQdRUdtXil3ScvQJtfShQ0bHnMf4f4pzp1BZC1DqCwgHF+Iw/gpJ0zFxmpaLhBm6JR3nue0huu1rt7Q==-----END PKCS7-----
+				">
+				<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+				<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+				</form>
+
               <a href="http://srv.buysellads.com/direct/click/track/yes/x/G6NDC27ICWAI627UCWALYK7UCKBILKJMCT7I5" onclick="_gaq.push(['_trackEvent', 'banner', 'click', 'treehouse']);" rel="nofollow" target="_blank">
                 <h4 style="margin-bottom: 0.4em;">Treehouse</h4>
                 <div class="clearfix">
@@ -90,6 +103,23 @@
                   <img src="/images/logo_with_parrot.png" width="175" height="135">
                 </span>
               </a>
+              <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+				<input type="hidden" name="cmd" value="_xclick">
+				<input type="hidden" name="business" value="G5N6M7TKREJ8G">
+				<input type="hidden" name="lc" value="US">
+				<input type="hidden" name="item_name" value="Margarita Booth Entry">
+				<input type="hidden" name="item_number" value="1">
+				<input type="hidden" name="amount" value="350.00">
+				<input type="hidden" name="currency_code" value="USD">
+				<input type="hidden" name="button_subtype" value="services">
+				<input type="hidden" name="bn" value="PP-BuyNowBF:btn_buynowCC_LG.gif:NonHostedGuest">
+				<table>
+				<tr><td><input type="hidden" name="on0" value="Margarita Booth Entry"><p>Margarita Booth Entry</p></td></tr><tr><td><input type="text" name="os0" maxlength="200"></td></tr>
+				</table>
+				<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+				<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+				</form>
+
               <a href="http://srv.buysellads.com/direct/click/track/yes/x/G6NDC27ICWAI627UCWALYK7UCKBILKJMCT7I5" onclick="_gaq.push(['_trackEvent', 'banner', 'click', 'treehouse']);" rel="nofollow" target="_blank">
                 <h4 style="margin-bottom: 0.4em;">Treehouse</h4>
                 <div class="clearfix">
@@ -107,6 +137,17 @@
                   <img src="/images/logo_with_parrot.png" width="175" height="135">
                 </span>
               </a>
+					<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+					<input type="hidden" name="cmd" value="_s-xclick">
+					<table>
+					<tr><td><input type="hidden" name="on0" value="Raffle Ticket"><p>Raffle Ticket</p></td></tr><tr><td><input type="text" name="os0" maxlength="200"></td></tr>
+					</table>
+					<input type="hidden" name="encrypted" value="-----BEGIN PKCS7-----MIIHbwYJKoZIhvcNAQcEoIIHYDCCB1wCAQExggEwMIIBLAIBADCBlDCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20CAQAwDQYJKoZIhvcNAQEBBQAEgYCF193l7eozeVXywKXkS7z+wkz1lThUuFpfwjO8k7UUG6+NyUnQYSzZ9g6QyI3JCZ7WN5vWXyXZuaVzfK8uFWQm34iJs5DuqpCHo80FSbPjxms6LTDGH1dqH9v9l64vihGNumn6EVN2pGSwv3pvYPyRjmWaHICmgEMmSUxs2vjydTELMAkGBSsOAwIaBQAwgewGCSqGSIb3DQEHATAUBggqhkiG9w0DBwQIIO98caiVqduAgcjDsuFk9mTO7/IR4f4qVkGkDvoASHhly0SalHWxDQvNossQPtxNteDpTnoYGfHD5w4t1pblF+JeV5uAn/V4rJOW8AWw7Ju6CzEHJbi5saLZ7VLtGum02JTOCRiEoNL74Npwadftdvqtju4IH+VeGpP7mMEMCJ9IuUXFnlYQqiH0rLqijgjAnQMArqniyDVhDaCJP4e01akPCvOeudSllB/aUMwocHh99NHmQE6xDN9Pc042UlFemFMKikA800WXQXxoxDbwVMUGrKCCA4cwggODMIIC7KADAgECAgEAMA0GCSqGSIb3DQEBBQUAMIGOMQswCQYDVQQGEwJVUzELMAkGA1UECBMCQ0ExFjAUBgNVBAcTDU1vdW50YWluIFZpZXcxFDASBgNVBAoTC1BheVBhbCBJbmMuMRMwEQYDVQQLFApsaXZlX2NlcnRzMREwDwYDVQQDFAhsaXZlX2FwaTEcMBoGCSqGSIb3DQEJARYNcmVAcGF5cGFsLmNvbTAeFw0wNDAyMTMxMDEzMTVaFw0zNTAyMTMxMDEzMTVaMIGOMQswCQYDVQQGEwJVUzELMAkGA1UECBMCQ0ExFjAUBgNVBAcTDU1vdW50YWluIFZpZXcxFDASBgNVBAoTC1BheVBhbCBJbmMuMRMwEQYDVQQLFApsaXZlX2NlcnRzMREwDwYDVQQDFAhsaXZlX2FwaTEcMBoGCSqGSIb3DQEJARYNcmVAcGF5cGFsLmNvbTCBnzANBgkqhkiG9w0BAQEFAAOBjQAwgYkCgYEAwUdO3fxEzEtcnI7ZKZL412XvZPugoni7i7D7prCe0AtaHTc97CYgm7NsAtJyxNLixmhLV8pyIEaiHXWAh8fPKW+R017+EmXrr9EaquPmsVvTywAAE1PMNOKqo2kl4Gxiz9zZqIajOm1fZGWcGS0f5JQ2kBqNbvbg2/Za+GJ/qwUCAwEAAaOB7jCB6zAdBgNVHQ4EFgQUlp98u8ZvF71ZP1LXChvsENZklGswgbsGA1UdIwSBszCBsIAUlp98u8ZvF71ZP1LXChvsENZklGuhgZSkgZEwgY4xCzAJBgNVBAYTAlVTMQswCQYDVQQIEwJDQTEWMBQGA1UEBxMNTW91bnRhaW4gVmlldzEUMBIGA1UEChMLUGF5UGFsIEluYy4xEzARBgNVBAsUCmxpdmVfY2VydHMxETAPBgNVBAMUCGxpdmVfYXBpMRwwGgYJKoZIhvcNAQkBFg1yZUBwYXlwYWwuY29tggEAMAwGA1UdEwQFMAMBAf8wDQYJKoZIhvcNAQEFBQADgYEAgV86VpqAWuXvX6Oro4qJ1tYVIT5DgWpE692Ag422H7yRIr/9j/iKG4Thia/Oflx4TdL+IFJBAyPK9v6zZNZtBgPBynXb048hsP16l2vi0k5Q2JKiPDsEfBhGI+HnxLXEaUWAcVfCsQFvd2A1sxRr67ip5y2wwBelUecP3AjJ+YcxggGaMIIBlgIBATCBlDCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20CAQAwCQYFKw4DAhoFAKBdMBgGCSqGSIb3DQEJAzELBgkqhkiG9w0BBwEwHAYJKoZIhvcNAQkFMQ8XDTE0MDQxMDAzMDM0NFowIwYJKoZIhvcNAQkEMRYEFCDt16mS8TRw8G0JARcBk7kn+2asMA0GCSqGSIb3DQEBAQUABIGABwEp6d2nmontJzd4oB6D/VPMhK9h3PGODzQOQiQ2ObSWXqHqi3PXiuZObsvjzokHTT4RO24rTd/5PvZBwgXLLoSLp2B+2X+5RiDD5yF4pXZOO2MhpBfDGcdnGcBj4PnP6ACJAdmCDWjRcZxtCHdlrkxu6mrxU2eHAGBKqoJA2M4=-----END PKCS7-----">
+					<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+					<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+					</form>
+
+
               <a href="http://srv.buysellads.com/direct/click/track/yes/x/G6NDC27ICWAI627UCWALYK7UCKBILKJMCT7I5" onclick="_gaq.push(['_trackEvent', 'banner', 'click', 'treehouse']);" rel="nofollow" target="_blank">
                 <h4 style="margin-bottom: 0.4em;">Treehouse</h4>
                 <div class="clearfix">
@@ -122,6 +163,17 @@
                   <img src="/images/logo_with_parrot.png" width="175" height="135">
                 </span>
               </a>
+             		<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+					<input type="hidden" name="cmd" value="_s-xclick">
+					<table>
+					<tr><td><input type="hidden" name="on0" value="5 Raffle Tickets for $20"><p>5 Raffle Tickets for $20</p></td></tr><tr><td><input type="text" name="os0" maxlength="200"></td></tr>
+					</table>
+					<input type="hidden" name="encrypted" value="-----BEGIN PKCS7-----MIIHmAYJKoZIhvcNAQcEoIIHiTCCB4UCAQExggEwMIIBLAIBADCBlDCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20CAQAwDQYJKoZIhvcNAQEBBQAEgYASKBr3/dty3RnZs63Yot4ZegZrTjuB1j4FZjWZMLrzkwKyeQAW2uEzUaQjrjfrdTSfthiBYGPXaecdTspmMf7X3YQjmHcOLW75JNydCjZkFNBzH9BpatRUSmcY0/tuBk/fKZrWYJ7i3z1c6QPkzhJGIEsdl2UU+FvgSqUQRuLCOzELMAkGBSsOAwIaBQAwggEUBgkqhkiG9w0BBwEwFAYIKoZIhvcNAwcECMHuBr1dKCPhgIHwTOw20+2a3oO4Nmtk915X4OMJYqFbUVfBwwga6i2n+lTswv1poJYC4UhoISbf31gakm7y+xGYWCkaYbdf7WHyJtznIAfgTc6DTLyVJUGvBxgBWFIZK4L4jzFLIEPC59gvvdPGs03U3l3bncVWpCuChqoaa8FOzfdL59uwKjCmg2VU6QT2d5IexO1mJ9lNKPU4uU5lJb/0ItLRXallwX41o+IzLqI5euTGf2I0w8DKp2yseMRosC+GiTyZWtMLXxoYnZeuScIrrm6Fb2KQePmFi7U9WSDaaLl22uIQD27eHLuH3XBJQdUimJF++SqJLjzaoIIDhzCCA4MwggLsoAMCAQICAQAwDQYJKoZIhvcNAQEFBQAwgY4xCzAJBgNVBAYTAlVTMQswCQYDVQQIEwJDQTEWMBQGA1UEBxMNTW91bnRhaW4gVmlldzEUMBIGA1UEChMLUGF5UGFsIEluYy4xEzARBgNVBAsUCmxpdmVfY2VydHMxETAPBgNVBAMUCGxpdmVfYXBpMRwwGgYJKoZIhvcNAQkBFg1yZUBwYXlwYWwuY29tMB4XDTA0MDIxMzEwMTMxNVoXDTM1MDIxMzEwMTMxNVowgY4xCzAJBgNVBAYTAlVTMQswCQYDVQQIEwJDQTEWMBQGA1UEBxMNTW91bnRhaW4gVmlldzEUMBIGA1UEChMLUGF5UGFsIEluYy4xEzARBgNVBAsUCmxpdmVfY2VydHMxETAPBgNVBAMUCGxpdmVfYXBpMRwwGgYJKoZIhvcNAQkBFg1yZUBwYXlwYWwuY29tMIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDBR07d/ETMS1ycjtkpkvjXZe9k+6CieLuLsPumsJ7QC1odNz3sJiCbs2wC0nLE0uLGaEtXynIgRqIddYCHx88pb5HTXv4SZeuv0Rqq4+axW9PLAAATU8w04qqjaSXgbGLP3NmohqM6bV9kZZwZLR/klDaQGo1u9uDb9lr4Yn+rBQIDAQABo4HuMIHrMB0GA1UdDgQWBBSWn3y7xm8XvVk/UtcKG+wQ1mSUazCBuwYDVR0jBIGzMIGwgBSWn3y7xm8XvVk/UtcKG+wQ1mSUa6GBlKSBkTCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb22CAQAwDAYDVR0TBAUwAwEB/zANBgkqhkiG9w0BAQUFAAOBgQCBXzpWmoBa5e9fo6ujionW1hUhPkOBakTr3YCDjbYfvJEiv/2P+IobhOGJr85+XHhN0v4gUkEDI8r2/rNk1m0GA8HKddvTjyGw/XqXa+LSTlDYkqI8OwR8GEYj4efEtcRpRYBxV8KxAW93YDWzFGvruKnnLbDAF6VR5w/cCMn5hzGCAZowggGWAgEBMIGUMIGOMQswCQYDVQQGEwJVUzELMAkGA1UECBMCQ0ExFjAUBgNVBAcTDU1vdW50YWluIFZpZXcxFDASBgNVBAoTC1BheVBhbCBJbmMuMRMwEQYDVQQLFApsaXZlX2NlcnRzMREwDwYDVQQDFAhsaXZlX2FwaTEcMBoGCSqGSIb3DQEJARYNcmVAcGF5cGFsLmNvbQIBADAJBgUrDgMCGgUAoF0wGAYJKoZIhvcNAQkDMQsGCSqGSIb3DQEHATAcBgkqhkiG9w0BCQUxDxcNMTQwNDEwMDMwNDM3WjAjBgkqhkiG9w0BCQQxFgQUdr+PSBGLiY7b/NNUcF8CDn+v8HwwDQYJKoZIhvcNAQEBBQAEgYBHwYB9WWzwKkbIjoaar3sgaYhuH/T2JPMdnYaKTqiuZ07CciVRYG3krJnhNpLJx7WNrZoJFP/QwuQvymTlNxws27TQ5iGMpxswmyTrumMSVAvEhhyiTCIuQYwIeWJxZetEFLsqebCzHdHyE43djyJZH0305SjQdpWbRdetT0KqYg==-----END PKCS7-----
+					">
+					<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+					<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+					</form>
+
               <a href="http://srv.buysellads.com/direct/click/track/yes/x/G6NDC27ICWAI627UCWALYK7UCKBILKJMCT7I5" onclick="_gaq.push(['_trackEvent', 'banner', 'click', 'treehouse']);" rel="nofollow" target="_blank">
                 <h4 style="margin-bottom: 0.4em;">Treehouse</h4>
                 <div class="clearfix">
