c038809a46bfe77c79295ee7caa7cc94eb0bae41
ryanorsinger <ryan.orsinger@gmail.com>
2014-04-10 08:20:27 -0500

    first real draft

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -27,23 +27,20 @@
 				<div class="navbar-collapse collapse" id="navbar-main">
 					<ul class="nav navbar-nav">
 						
-						
+						</li>
+						<li>
 							
+							<a href="#top">Stir up some fun!</a>
 						</li>
 						<li>
-							<a href="../help/">Help</a>
+							<a href="#our_story">Our Story</a>
 						</li>
-						<li>
-							<a href="http://news.bootswatch.com">Blog</a>
-						</li>
-						<li>
-							<a class="dropdown-toggle" data-toggle="dropdown" href="#" id="download">Download <span class="caret"></span></a>
-						</li>
+						
 					</ul>
 
 					<ul class="nav navbar-nav navbar-right">
-						<li><a href="http://builtwithbootstrap.com/" target="_blank">Built With Bootstrap</a></li>
-						<li><a href="https://www.facebook.com/ahakwheritage"><i class="fa fa-facebook-square"></i>&nbsp;AHA Facebook</li>
+						
+						<li><a href="https://www.facebook.com/ahakwheritage"><i class="fa fa-facebook-square"></i>&nbsp;Connect with us on Facebook</li>
 					</ul>
 
 				</div>
@@ -51,34 +48,18 @@
 		</div>
 
 
-			<footer>
-				<div class="row">
-					<div class="col-lg-12">
-
-						<ul class="list-unstyled">
-							<li class="pull-right"><a href="#top">Back to top</a></li>
-							<li><a href="http://news.bootswatch.com" onclick="pageTracker._link(this.href); return false;">Blog</a></li>
-							<li><a href="http://feeds.feedburner.com/bootswatch">RSS</a></li>
-							<li><a href="https://twitter.com/thomashpark">Twitter</a></li>
-							<li><a href="https://github.com/thomaspark/bootswatch/">GitHub</a></li>
-							<li><a href="../help/#api">API</a></li>
-							<li><a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&amp;hosted_button_id=F22JEM3Q78JC2">Donate</a></li>
-						</ul>
-						<p>Made by <a href="http://thomaspark.me" rel="nofollow">Thomas Park</a>. Contact him at <a href="mailto:thomas@bootswatch.com">thomas@bootswatch.com</a>.</p>
-						<p>Code released under the <a href="https://github.com/thomaspark/bootswatch/blob/gh-pages/LICENSE">MIT License</a>.</p>
-						<p>Based on <a href="http://getbootstrap.com" rel="nofollow">Bootstrap</a>. Icons from <a href="http://fortawesome.github.io/Font-Awesome/" rel="nofollow">Font Awesome</a>. Web fonts from <a href="http://www.google.com/webfonts" rel="nofollow">Google</a>.</p>
-
-					</div>
-				</div>
-
-			</footer>
+			
 
 
 		</div>
 
+<center>
+<img src="/images/contact.png">
+</center>
+
 		<script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
 		<script src="../bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
-		<script src="../assets/js/bootswatch.js"></script>*/
+		<script src="../assets/js/bootswatch.js"></script>
  
 
 
